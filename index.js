351d9cd478790aff940c6d517b6620d25212d50d
zjilani <zaidjilani.09@gmail.com>
2020-09-12 18:06:27 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+
+const app = express()
+
+connectDB();
+
+app.use(express.json({ extended : false }));
+
+app.use('/',require('./routes/index'));
+app.use('/api/url',require('./routes/url'));
+
+const PORT = 3000;
+
+app.listen(PORT , ()=> {
+    console.log("Server is running at Port "+PORT);
+})
